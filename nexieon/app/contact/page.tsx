0004c73ec44b80807cdbfc5e4e4099115c045d32
AwsomeWorkDevoloper <sashema09@gmail.com>
2023-06-25 01:01:43 -0400

    Added functionality to contact page, connected to mongodb through prisma

--- nexieon/app/contact/page.tsx
+++ nexieon/app/contact/page.tsx
@@ -1,10 +1,42 @@
 'use client';
 
-import Image from "next/image";
+import axios from "axios";
 import { useRouter } from "next/navigation";
+import { useState } from "react";
+import Swal from "sweetalert2";
 
 const Contact = () => {
   const router = useRouter();
+
+  const [name, setName] = useState('');
+  const [company, setCompany] = useState('');
+  const [email, setEmail] = useState('');
+  const [phone, setPhone] = useState('');
+  const [msg, setMsg] = useState('');
+
+  const sendMessage = async (e:any) => {
+    e.preventDefault();
+
+    const data = {
+        name, company, email, phone, message: msg
+    };
+
+    console.log(`Data: ${data}`);
+    
+
+    const res = await axios.post('/api/messages/send', {...data});
+    const resData = await res.data;
+
+    console.log(`resData: ${resData}`);
+
+    Swal.fire({
+        icon: 'success',
+        title: 'Thank you!',
+        text: 'We\'ll try to get back to you as soon as we can!'
+    }).then(() => {
+        router.refresh();
+    });
+  }
 
   return (
     <>
@@ -22,21 +54,21 @@ const Contact = () => {
                         Connect with us to help you take that next step in achieving your goals.
                     </p>
                 </div>
-                <form className="w-full md:w-1/2 fadeInAnim">
+                <form className="w-full md:w-1/2 fadeInAnim" onSubmit={sendMessage}>
                     <div className='p-2 w-full'>
-                        <input type="text" placeholder="Name" required/>
+                        <input type="text" placeholder="Name" required onChange={(e) => setName(e.target.value)}/>
                     </div>
                     <div className='p-2 w-full'>
-                        <input type="text" placeholder="Company" required/>
+                        <input type="text" placeholder="Company" required onChange={(e) => setCompany(e.target.value)}/>
                     </div>
                     <div className='p-2 w-full'>
-                        <input type="email" placeholder="Email" required/>
+                        <input type="email" placeholder="Email" required onChange={(e) => setEmail(e.target.value)}/>
                     </div>
                     <div className='p-2 w-full'>
-                        <input type="tel" placeholder="Phone" />
+                        <input type="tel" placeholder="Phone" onChange={(e) => setPhone(e.target.value)}/>
                     </div>
                     <div className='p-2 w-full'>
-                        <textarea placeholder="Your Message..." cols={30} rows={8} required></textarea>
+                        <textarea placeholder="Your Message..." cols={30} rows={8} required onChange={(e) => setMsg(e.target.value)}></textarea>
                     </div>
                     <div className="p-2 w-full">
                         <button className = 'btn-secondary w-full mt-5' type="submit">Send</button>
