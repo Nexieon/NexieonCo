a642dfed3b13872bcf9e1bfdc8d7b0bd451b01f2
Mose Run <moserun@Moses-Air.hitronhub.home>
2023-08-19 18:13:22 -0400

    re-worked work page, not 100% complete but mostly functional and presentable, also changed home bg

---
+++ nexieon/app/components/Examplebox.tsx
@@ -0,0 +1,125 @@
+'use client'
+
+import { useState } from "react";
+import Image from "next/image";
+
+type Props = {
+  src: string;
+  isPictures: boolean;
+  isLink: boolean;
+  link?: string
+  name: string;
+  onImageViewerEnabled?: (flag: boolean) => void 
+  onSrcTransfer?: (src: string) => void;
+};
+
+const Examplebox = ({ src, isPictures, isLink, link, name, onImageViewerEnabled, onSrcTransfer }: Props) => {
+  const [ImageViewerFlag, setImageViewerFlag] = useState(false)
+
+  const handleFlagChange = (newFlag: boolean) => {
+    setImageViewerFlag(newFlag);
+    if (onImageViewerEnabled) {
+      onImageViewerEnabled(newFlag)
+    }
+  };
+
+  return (
+    <div className="shadow-2xl p-4 rounded-xl bg-lightmode-gray">
+      <div className="group relative">
+        <div className=" w-full bg-black rounded-xl">
+          <Image
+            width={550}
+            height={550}
+            alt={name}
+            className="rounded-lg h-[18rem] object-cover w-full  transition-opacity duration-300 group-hover:opacity-40 hover:opacity-100"
+            src={src}
+          />
+          <div className=" absolute inset-0 flex items-center justify-center opacity-0 group-hover:opacity-100 transition-opacity flex-col">
+            <div className="flex gap-3 mb-4">
+              <a
+                target = '_blank'
+                href={link}
+                className="text-black bg-white p-4 rounded-xl hover:bg-zinc-300"
+                style={isLink ? { display: "block" } : { display: "none" }}
+              >
+                <svg
+                  viewBox="0 0 1024 1024"
+                  fill="currentColor"
+                  height="1.4em"
+                  width="1.4em"
+                >
+                  <path d="M574 665.4a8.03 8.03 0 00-11.3 0L446.5 781.6c-53.8 53.8-144.6 59.5-204 0-59.5-59.5-53.8-150.2 0-204l116.2-116.2c3.1-3.1 3.1-8.2 0-11.3l-39.8-39.8a8.03 8.03 0 00-11.3 0L191.4 526.5c-84.6 84.6-84.6 221.5 0 306s221.5 84.6 306 0l116.2-116.2c3.1-3.1 3.1-8.2 0-11.3L574 665.4zm258.6-474c-84.6-84.6-221.5-84.6-306 0L410.3 307.6a8.03 8.03 0 000 11.3l39.7 39.7c3.1 3.1 8.2 3.1 11.3 0l116.2-116.2c53.8-53.8 144.6-59.5 204 0 59.5 59.5 53.8 150.2 0 204L665.3 562.6a8.03 8.03 0 000 11.3l39.8 39.8c3.1 3.1 8.2 3.1 11.3 0l116.2-116.2c84.5-84.6 84.5-221.5 0-306.1zM610.1 372.3a8.03 8.03 0 00-11.3 0L372.3 598.7a8.03 8.03 0 000 11.3l39.6 39.6c3.1 3.1 8.2 3.1 11.3 0l226.4-226.4c3.1-3.1 3.1-8.2 0-11.3l-39.5-39.6z" />
+                </svg>
+              </a>
+
+              <button
+                onClick = {() => {
+                  handleFlagChange(true)
+                  if (onSrcTransfer !== undefined) {
+                    onSrcTransfer(src)
+                  }
+                }}
+                className="text-black bg-white p-4 rounded-xl hover:bg-zinc-300 "
+                style={isPictures ? { display: "block" } : { display: "none" }}
+              >
+                <svg
+                  viewBox="0 0 576 512"
+                  fill="currentColor"
+                  height="1.4em"
+                  width="1.4em"
+                >
+                  <path d="M512 32H160c-35.35 0-64 28.65-64 64v224c0 35.35 28.65 64 64 64h352c35.35 0 64-28.65 64-64V96c0-35.35-28.7-64-64-64zm16 288c0 8.822-7.178 16-16 16h-16L386.7 175.1c-3-4.4-8-7.1-13.4-7.1a15.978 15.978 0 00-13.31 7.125l-62.74 94.11L274.9 238.6c-3-4.2-7.8-6.6-12.9-6.6a16.007 16.007 0 00-12.93 6.574L176 336h-16c-8.822 0-16-7.178-16-16V96c0-8.822 7.178-16 16-16h352c8.822 0 16 7.178 16 16v224zM224 112c-17.67 0-32 14.33-32 32s14.33 32 32 32c17.68 0 32-14.33 32-32s-14.3-32-32-32zm232 368H120C53.83 480 0 426.2 0 360V120c0-13.2 10.75-24 24-24s24 10.8 24 24v240c0 39.7 32.3 72 72 72h336c13.25 0 24 10.75 24 24s-10.7 24-24 24z" />
+                </svg>
+              </button>
+            </div>
+            <span className="text-white text-2xl font-semibold ">
+              {name}
+            </span>
+          </div>
+        </div>
+      </div>
+
+      <div className="flex mx-3 mt-3">
+        <span className="font-semibold text-md text-black mr-auto">{name}</span>
+
+        <a
+          target = '_blank'
+          href={link}
+          className="text-black hover:text-primary-blue  mr-4"
+          style={isLink ? { display: "block" } : { display: "none" }}
+        >
+          <svg
+            viewBox="0 0 1024 1024"
+            fill="currentColor"
+            height="1.4em"
+            width="1.4em"
+          >
+            <path d="M574 665.4a8.03 8.03 0 00-11.3 0L446.5 781.6c-53.8 53.8-144.6 59.5-204 0-59.5-59.5-53.8-150.2 0-204l116.2-116.2c3.1-3.1 3.1-8.2 0-11.3l-39.8-39.8a8.03 8.03 0 00-11.3 0L191.4 526.5c-84.6 84.6-84.6 221.5 0 306s221.5 84.6 306 0l116.2-116.2c3.1-3.1 3.1-8.2 0-11.3L574 665.4zm258.6-474c-84.6-84.6-221.5-84.6-306 0L410.3 307.6a8.03 8.03 0 000 11.3l39.7 39.7c3.1 3.1 8.2 3.1 11.3 0l116.2-116.2c53.8-53.8 144.6-59.5 204 0 59.5 59.5 53.8 150.2 0 204L665.3 562.6a8.03 8.03 0 000 11.3l39.8 39.8c3.1 3.1 8.2 3.1 11.3 0l116.2-116.2c84.5-84.6 84.5-221.5 0-306.1zM610.1 372.3a8.03 8.03 0 00-11.3 0L372.3 598.7a8.03 8.03 0 000 11.3l39.6 39.6c3.1 3.1 8.2 3.1 11.3 0l226.4-226.4c3.1-3.1 3.1-8.2 0-11.3l-39.5-39.6z" />
+          </svg>
+        </a>
+
+        <button
+          className="text-black hover:text-primary-blue "
+          style={isPictures ? { display: "block",  } : { display: "none",}}
+          onClick = {() => {
+            handleFlagChange(true)
+            if (onSrcTransfer !== undefined) {
+              onSrcTransfer(src)
+            }
+          }}
+        >
+          <svg
+            viewBox="0 0 576 512"
+            fill="currentColor"
+            height="1.4em"
+            width="1.4em"
+          >
+            <path d="M512 32H160c-35.35 0-64 28.65-64 64v224c0 35.35 28.65 64 64 64h352c35.35 0 64-28.65 64-64V96c0-35.35-28.7-64-64-64zm16 288c0 8.822-7.178 16-16 16h-16L386.7 175.1c-3-4.4-8-7.1-13.4-7.1a15.978 15.978 0 00-13.31 7.125l-62.74 94.11L274.9 238.6c-3-4.2-7.8-6.6-12.9-6.6a16.007 16.007 0 00-12.93 6.574L176 336h-16c-8.822 0-16-7.178-16-16V96c0-8.822 7.178-16 16-16h352c8.822 0 16 7.178 16 16v224zM224 112c-17.67 0-32 14.33-32 32s14.33 32 32 32c17.68 0 32-14.33 32-32s-14.3-32-32-32zm232 368H120C53.83 480 0 426.2 0 360V120c0-13.2 10.75-24 24-24s24 10.8 24 24v240c0 39.7 32.3 72 72 72h336c13.25 0 24 10.75 24 24s-10.7 24-24 24z" />
+          </svg>
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Examplebox;
--- nexieon/app/page.tsx
+++ nexieon/app/page.tsx
@@ -16,7 +16,7 @@ export default function Home() {
           autoPlay
           loop
           muted
-          src="./roller.mp4"
+          src="./fastdrone.mp4"
           className="h-[100vh] w-full object-cover"
         />
 
@@ -27,7 +27,7 @@ export default function Home() {
                 "text-6xl md:text-7xl lg:text-8xl font-bold p-6 fadeInAnim"
               }
             >
-              <h2>Software for your comfort.</h2>
+              <h1>Software for your comfort.</h1>
             </div>
 
             <div className="fadeInAnim mt-14 gap-6 flex justify-center">
@@ -95,7 +95,6 @@ export default function Home() {
               <p className="text-white mt-3">
                 Devoted marketing designed in one place to perfect your image.
               </p>
-              {/*<button className = 'btn-main-reverse w-full lg:w-auto my-auto' >See More</button>*/}
             </div>
             <div className="hidden md:block">
               <Image
@@ -152,7 +151,7 @@ export default function Home() {
             >
               <path d="M21.809 5.524L12.806.179l-.013-.007.078-.045h-.166a1.282 1.282 0 00-1.196.043l-.699.403-8.604 4.954a1.285 1.285 0 00-.644 1.113v10.718c0 .46.245.884.644 1.113l9.304 5.357c.402.232.898.228 1.297-.009l9.002-5.345c.39-.231.629-.651.629-1.105V6.628c0-.453-.239-.873-.629-1.104zm-19.282.559L11.843.719a.642.642 0 01.636.012l9.002 5.345a.638.638 0 01.207.203l-4.543 2.555-4.498-2.7a.963.963 0 00-.968-.014L6.83 8.848 2.287 6.329a.644.644 0 01.24-.246zm14.13 8.293l-4.496-2.492V6.641a.32.32 0 01.155.045l4.341 2.605v5.085zm-4.763-1.906l4.692 2.601-4.431 2.659-4.648-2.615a.317.317 0 01-.115-.112l4.502-2.533zm-.064 10.802l-9.304-5.357a.643.643 0 01-.322-.557V7.018L6.7 9.51v5.324c0 .348.188.669.491.84l4.811 2.706.157.088v4.887a.637.637 0 01-.329-.083z" />
             </svg>
-            <h1 className="text-[#1360f0] text-3xl font-bold ">Fully Custom</h1>
+            <h2 className="text-darkmode-gray text-3xl font-bold ">Fully Custom</h2>
             <p className="text-center mx-10 mt-5 text-black">
               Tailored solutions, covering backend development to frontend
               design, empowering you to truly make it your own.
@@ -169,7 +168,7 @@ export default function Home() {
             >
               <path d="M6.5 13a6.474 6.474 0 003.845-1.258h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 001.415-1.414l-3.85-3.85a1.008 1.008 0 00-.115-.1A6.471 6.471 0 0013 6.5 6.502 6.502 0 006.5 0a6.5 6.5 0 100 13zm0-8.518c1.664-1.673 5.825 1.254 0 5.018-5.825-3.764-1.664-6.69 0-5.018z" />
             </svg>
-            <h1 className="text-[#1360f0] text-3xl font-bold ">SEO</h1>
+            <h2 className="text-darkmode-gray text-3xl font-bold ">SEO</h2>
             <p className="text-center mx-10 mt-5 text-black">
               Meticulously crafted websites, following industry best practices
               for high search engine rankings and increased web traffic.
@@ -186,7 +185,7 @@ export default function Home() {
             >
               <path d="M11.251.068a.5.5 0 01.227.58L9.677 6.5H13a.5.5 0 01.364.843l-8 8.5a.5.5 0 01-.842-.49L6.323 9.5H3a.5.5 0 01-.364-.843l8-8.5a.5.5 0 01.615-.09z" />
             </svg>
-            <h1 className="text-[#1360f0] text-3xl font-bold ">Fast</h1>
+            <h2 className="text-darkmode-gray text-3xl font-bold ">Fast</h2>
             <p className="text-center mx-10 mt-5 text-black">
               Using cutting-edge technology, we build lightning-fast websites
               for unparalleled user experiences.
@@ -202,9 +201,9 @@ export default function Home() {
             >
               <path d="M470.7 9.4c3 3.1 5.3 6.6 6.9 10.3s2.4 7.8 2.4 12.2V128c0 17.7-14.3 32-32 32s-32-14.3-32-32v-18.7L310.6 214.6c-11.8 11.8-30.8 12.6-43.5 1.7L176 138.1l-91.2 78.2c-13.4 11.5-33.6 9.9-45.1-3.5s-9.9-33.6 3.5-45.1l112-96c12-10.3 29.7-10.3 41.7 0l89.5 76.7L370.7 64H352c-17.7 0-32-14.3-32-32s14.3-32 32-32h96c8.8 0 16.8 3.6 22.6 9.3l.1.1zM0 304c0-26.5 21.5-48 48-48h416c26.5 0 48 21.5 48 48v160c0 26.5-21.5 48-48 48H48c-26.5 0-48-21.5-48-48V304zm48 112v48h48c0-26.5-21.5-48-48-48zm48-112H48v48c26.5 0 48-21.5 48-48zm368 112c-26.5 0-48 21.5-48 48h48v-48zm-48-112c0 26.5 21.5 48 48 48v-48h-48zm-96 80c0-35.3-28.7-64-64-64s-64 28.7-64 64 28.7 64 64 64 64-28.7 64-64z" />
             </svg>
-            <h1 className="text-[#1360f0] text-3xl font-bold ">
+            <h2 className="text-darkmode-gray text-3xl font-bold ">
               Cost Effective
-            </h1>
+            </h2>
             <p className="text-center mx-10 mt-5 text-black">
               Proudly offering cost-effective services at competitive prices, so
               you can focus on what truly matters for your success.
@@ -226,7 +225,7 @@ export default function Home() {
               <path d="M4 4 H20 A1 1 0 0 1 21 5 V15 A1 1 0 0 1 20 16 H4 A1 1 0 0 1 3 15 V5 A1 1 0 0 1 4 4 z" />
               <path d="M7 20h10M9 16v4M15 16v4M9 12V8M12 12v-1M15 12v-2M12 12v-1" />
             </svg>
-            <h1 className="text-[#1360f0] text-3xl font-bold ">Analytics</h1>
+            <h2 className="text-darkmode-gray text-3xl font-bold ">Analytics</h2>
             <p className="text-center mx-10 mt-5 text-black">
               Track analytics, optimize web performance with our advanced tools
               and expertise for peak digital efficiency.
@@ -243,7 +242,7 @@ export default function Home() {
             >
               <path d="M18 15h-2v2h2m0-6h-2v2h2m2 6h-8v-2h2v-2h-2v-2h2v-2h-2V9h8M10 7H8V5h2m0 6H8V9h2m0 6H8v-2h2m0 6H8v-2h2M6 7H4V5h2m0 6H4V9h2m0 6H4v-2h2m0 6H4v-2h2m6-10V3H2v18h20V7H12z" />
             </svg>
-            <h1 className="text-[#1360f0] text-3xl font-bold">Hosting</h1>
+            <h2 className="text-darkmode-gray text-3xl font-bold">Hosting</h2>
             <p className="text-center mx-10 mt-5 text-black">
               Seamless deployment, expert hosting, and integration into the vast
               internet – we take care of it all.
@@ -260,7 +259,7 @@ export default function Home() {
             >
               <path d="M315.4 15.5C309.7 5.9 299.2 0 288 0s-21.7 5.9-27.4 15.5l-96 160c-5.9 9.9-6.1 22.2-.4 32.2s16.3 16.2 27.8 16.2h192c11.5 0 22.2-6.2 27.8-16.2s5.5-22.3-.4-32.2l-96-160zM288 312v144c0 22.1 17.9 40 40 40h144c22.1 0 40-17.9 40-40V312c0-22.1-17.9-40-40-40H328c-22.1 0-40 17.9-40 40zM128 512c70.7 0 128-57.3 128-128s-57.3-128-128-128S0 313.3 0 384s57.3 128 128 128z" />
             </svg>
-            <h1 className="text-[#1360f0] text-3xl font-bold">Modern Design</h1>
+            <h2 className="text-darkmode-gray text-3xl font-bold">Modern Design</h2>
             <p className="text-center mx-10 mt-5 text-black">
               Modern design practices for a contemporary online presence and
               exceptional user experience.
@@ -277,9 +276,9 @@ export default function Home() {
             >
               <path d="M21 16V4H3v12h18m0-14a2 2 0 012 2v12a2 2 0 01-2 2h-7v2h2v2H8v-2h2v-2H3a2 2 0 01-2-2V4c0-1.11.89-2 2-2h18M5 6h9v5H5V6m10 0h4v2h-4V6m4 3v5h-4V9h4M5 12h4v2H5v-2m5 0h4v2h-4v-2z" />
             </svg>
-            <h1 className="text-[#1360f0] text-3xl font-bold">
+            <h2 className="text-darkmode-gray text-3xl font-bold">
               Owner dashboard
-            </h1>
+            </h2>
             <p className="text-center mx-10 mt-5 text-black">
               Custom owner's dashboard for effortless site updates and tracking.
             </p>
@@ -289,9 +288,9 @@ export default function Home() {
 
       <article className="bg-white py-20 px-10 lg:px-[15%]">
         <div className="w-full lg:w-3/4 fadeInAnim">
-          <h1 className="text-black text-2xl lg:text-4xl font-extrabold mb-7 flex items-center ">
+          <h2 className="text-black text-2xl lg:text-4xl font-extrabold mb-7 flex items-center ">
             Driven by excellence, fueled by passion
-          </h1>
+          </h2>
           {/* <div className = 'flex text-2xl lg:text-4xl font-extrabold mb-7 gap-2'>
             <h1 className = 'text-black'>Driven by</h1><h1 className = 'text-primary-blue'>excellence,</h1><h1 className = 'text-black'>fueled by </h1><h1 className = 'text-primary-blue'>passion</h1>
           </div> */}
